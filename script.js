7011e094c07ef3945395522d216ad891d23a69c0
Abhinandan Das <abhinandandas77@gmail.com>
2024-03-06 03:38:02 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+// script.js
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+let slideIndex = 1;
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+function readMore(ele, eve) {
+    eve.preventDefault();
+    var txt = document.querySelector(".read_more_text");
+    txt.classList.toggle("show-few-lines");
+
+    if (txt.classList.contains("show-few-lines")) {
+        ele.innerText = "...";
+    } else {
+        ele.innerText = "...";
+    }
+}
+
+showSlides();
